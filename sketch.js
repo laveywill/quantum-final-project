6e070dedaba2e6984dc546d7049eaa08561ff5ae
Will Lavey <wlavey@middlebury.edu>
2024-11-19 12:37:20 -0500

    trying to add filters

--- sketch.js
+++ sketch.js
@@ -1,13 +1,15 @@
 let photons = [];
 let filters = [];
+let mirrors = [];
 
 function setup() {
   createCanvas(1000, 800);
   angleMode(RADIANS);
   noStroke();
 
-  photons.push(new Photon(createVector(width/2, height/2), createVector(1 / sqrt(2), 1 / sqrt(2))));
+  photons.push(new Photon(createVector(width/2, height/2), createVector(1 / sqrt(2), 1 / sqrt(2)), createVector(1, 0)));
   filters.push(new Filter(createVector(3 * width / 4, height / 2), createVector(1, 0)));
+  mirrors.push(new Mirror(createVector(2*width / 3, height / 2), PI/4));
 }
 
 function draw() {
@@ -23,10 +25,14 @@ function draw() {
   filters.forEach((f) => {
     f.draw();
   })
+
+  mirrors.forEach((m) => {
+    m.draw();
+  })
 }
 
 class Photon {
-  constructor(position, amplitudes) {
+  constructor(position, amplitudes, direction) {
     this.position = position
     if (round(amplitudes.magSq(), 4) !== 1) {
       print("ERROR: NOT A VALID STATE")
@@ -35,6 +41,7 @@ class Photon {
       this.rotation = asin(amplitudes.x) * (Math.sign(amplitudes.y));
     }
     this.absorbed = false;
+    this.direction = direction;
   }
 
   draw() {
@@ -52,18 +59,58 @@ class Photon {
   update() {
     if (this.position.x > width) {
       this.position.x = 0;
+    } else if (this.position.x < 0) {
+      this.position.x = width;
+    } else if (this.position.y > height) {
+      this.position.y = 0;
+    } else if (this.position.y < 0) {
+      this.position.y = height;
     } else {
-      this.position.x += 1;
+      this.position = this.position + this.direction;;
     }
 
     filters.forEach((f) => {
-      // print(p5.Vector.dist(f.position, this.position));
       if (p5.Vector.dist(f.position, this.position) < 10) {
         [this.absorbed, this.amplitudes] = measure(this, f);
         this.rotation = asin(this.amplitudes.x) * (Math.sign(this.amplitudes.y));
       }
     })
 
+    mirrors.forEach((m) => {
+      const distance = p5.Vector.dist(m.position, this.position);
+    
+      if (distance < 10) {
+        // Mirror's normal vector based on its angle
+        const normal = p5.Vector.fromAngle(m.angle).normalize();
+    
+        // Compute the reflection using the formula:
+        // r = d - 2 * (d . n) * n
+        const dotProduct = this.direction.dot(normal);
+        const reflection = this.direction.sub(p5.Vector.mult(normal, 2 * dotProduct));
+    
+        // Update the photon's direction directly
+        this.direction = reflection.normalize();
+      }
+    });
+
+  }
+}
+
+class Mirror {
+  constructor(position, angle) {
+    this.position = position;
+    this.angle = angle;
+  }
+
+  draw() {
+    stroke('grey');
+    strokeWeight(4);
+
+    push();
+    translate(this.position.x, this.position.y);
+    rotate(this.angle);
+    line(0, 0, 10, 0);
+    pop();
   }
 }
 
